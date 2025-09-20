e3493b6cccb5cf9aa5a5145f6880497f33b6b666
Sam Silver <sam55silver@gmail.com>
2025-09-20 14:26:31 -0400

    Update ArcticMap component to use CartoDB Voyager tiles for enhanced detail and water body labels, improving map presentation.

--- ui/src/components/ArcticMap.tsx
+++ ui/src/components/ArcticMap.tsx
@@ -42,9 +42,9 @@ export default function ArcticMap() {
           maxZoom: 18,
         });
 
-        // Add CartoDB Positron tiles (English labels, clean style)
+        // Add CartoDB Voyager tiles (more detailed with water body labels)
         L.tileLayer(
-          "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
+          "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png",
           {
             attribution:
               '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
